1d86805cff1e3066f66bc33d3249330f6e3789a9
Jsbro98 <jacobstanbrough@gmail.com>
2022-11-03 04:00:47 -0500

    adding more functions/selectors, added the input variable

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // starting by adding the selectors
-const buttons = Array.from(document.querySelectorAll('button'));
+const keypadButtons = Array.from(document.querySelectorAll('.keypad'));
+const equationButtons = Array.from(document.querySelectorAll('.equation'));
+const equalsButton = document.querySelector('.equals-button');
+const inputDisplay = document.querySelector('number-display');
 
 // adding the click event listener function
 const addClickListenser = (element, callback) => {
     return element.addEventListener('click', callback);
 };
 
+// master input
+let input = '';
+
+
 // creating some pure functions
+
+
 const makeNegative =(input) => {
     return -input;
 };
@@ -24,5 +33,16 @@ const multiply = (num1, num2) => {
 };
 
 const divide = (num1, num2) => {
+    if (num2 === 0)
     return num1 / num2;
 };
+
+const returnValue = (element) => {
+    return element.target.value;
+};
+
+const addToInput = (element) => {
+    console.log(input);
+    let value = returnValue(element);
+   return input += value;
+}
